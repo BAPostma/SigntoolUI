3e9009d8e64e9908693b12618ee9f43f4a98cbb3
Barry Postma <me@barrypostma.com>
2016-12-16 23:43:19 +0000

    Updated AssemblyInfo and version number to reflect new release

--- SignTool UI/Properties/AssemblyInfo.cs
+++ SignTool UI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Easy SignTool")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("EasySign")]
+[assembly: AssemblyDescription("Graphical User Inteface for signtool.exe")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Technodev")]
+[assembly: AssemblyCompany("Cyrix Solutions")]
 [assembly: AssemblyProduct("SignTool UI")]
-[assembly: AssemblyCopyright("Copyright © Technoberg BV 2013")]
+[assembly: AssemblyCopyright("Copyright © Cyrix Solutions 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
